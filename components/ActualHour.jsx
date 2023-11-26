75ba22ea1a54164baec362d7c0c53c9b5a2a7989
Paula Cano <paulacanoroman@gmail.com>
2023-11-26 12:36:01 +0100

    feat: component ActualHour

---
+++ components/ActualHour.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ActualHour = () => {
+  return (
+    <div>ActualHour</div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import { ActualHour } from '@/components/ActualHour'
 
 
 export default function Home() {
@@ -12,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        
+        <ActualHour></ActualHour>
       </main>
     </>
   )
