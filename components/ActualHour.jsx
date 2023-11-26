cad550fc00b42c181a7b7b1b0627ceef64523562
Paula Cano <paulacanoroman@gmail.com>
2023-11-26 12:38:24 +0100

    feat: useEffect for set Interval

--- components/ActualHour.jsx
+++ components/ActualHour.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const ActualHour = () => {
+
+    const [hour, setHour] = useState()
+
+    useEffect(() => {
+      const interval = setInterval(()=>{
+        const date = new Date()
+        console.log(date)
+      }, 1000)
+      interval
+    }, [])
+    
   return (
     <div>ActualHour</div>
   )
