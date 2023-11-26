0db4d9937c063c6b8faf3355d2345b99feebb778
Paula Cano <paulacanoroman@gmail.com>
2023-11-26 21:43:31 +0100

    feat: logic for delete first word

--- components/InputDeleteFirstWord.jsx
+++ components/InputDeleteFirstWord.jsx
@@ -3,11 +3,18 @@ import React, { useState } from 'react'
 const InputDeleteFirstWord = () => {
 
     const [userText, setUserText] = useState("")
+
+    const handleDeleteText = () => {
+        const newText = structuredClone(userText);
+        const words = newText.split(" ");
+        const deletedFirstWord = words.slice(1,-1).join(" ")
+        console.log(deletedFirstWord)
+    }
   return (
     <div>
-        {userText}
+        
         <input onChange={(e)=>{setUserText(e.target.value)}} placeholder='Write a sentence'></input>
-        <button onClick={()=>{}}>Delete first word</button>
+        <button onClick={()=>{handleDeleteText()}}>Delete first word</button>
         
     </div>
   )
