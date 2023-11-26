e73d78d7e08f535c9114f37e01514b0d8b2fc2a9
Paula Cano <paulacanoroman@gmail.com>
2023-11-26 12:46:40 +0100

    feat: actual date

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pt_261123
+
+Añade la fecha y hora actual que se actualice cada segundo
--- components/ActualHour.jsx
+++ components/ActualHour.jsx
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from 'react'
 
 export const ActualHour = () => {
 
-    const [hour, setHour] = useState()
+    const [hour, setHour] = useState("00:00:00")
+    const [date, setDate] = useState("DD/MM/YY")
 
     useEffect(() => {
       const interval = setInterval(()=>{
-            const date = new Date()
-            const onlyNumber = date.toLocaleString().split(",")[1]
-            console.log(onlyNumber)
+            const totalDate = new Date()
+            const onlyHour = totalDate.toLocaleString().split(",")[1]
+            const onlyDate = totalDate.toLocaleDateString().split(",")[0]
+            setHour(onlyHour)
+            setDate(onlyDate)
             }, 1000)
         return () => clearInterval(interval)
      
     }, [])
     
   return (
-    <div>{hour}</div>
+    <div>{date}{hour}</div>
   )
 }
