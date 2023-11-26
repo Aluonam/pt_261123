083f7c507354dfa3ea3d1c48dc378f53a668e28b
Paula Cano <paulacanoroman@gmail.com>
2023-11-26 21:36:48 +0100

    feat: button and input

--- components/InputDeleteFirstWord.jsx
+++ components/InputDeleteFirstWord.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const InputDeleteFirstWord = () => {
+
+    const [userText, setUserText] = useState("")
   return (
-    <div>InputDeleteFirstWord</div>
+    <div>
+        {userText}
+        <input onChange={(e)=>{setUserText(e.target.value)}} placeholder='Write a sentence'></input>
+        <button onClick={()=>{}}>Delete first word</button>
+        
+    </div>
   )
 }
 
